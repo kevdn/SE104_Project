86d04aea2b252a2f3ccf8a59cfb73e66135733c9
LAU TRUONG MINH <163316784+LiuChangMing88@users.noreply.github.com>
2024-05-24 00:17:26 +0700

    changed register form

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,4 +1,6 @@
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock, FaPhone, FaAddressCard } from "react-icons/fa";
+import { FaHouse } from "react-icons/fa6";
+import { MdOutlineDateRange } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 import './Register.css';
 
@@ -15,22 +17,37 @@ const Register = () => {
                 <form action="">
                     <h1>Đăng ký</h1>
                     <div className='register'></div>
-                    <div className='inputBox2'>
-                        <input type="text" placeholder="Họ" required=""/>
-                        <input type="text" placeholder="Tên" required=""/>
-                    </div>
                     <div className='inputBox'>
-                        <input type="text" placeholder="Email/Số điện thoại" required=""/>
-                        <FaUser className='icon'/>
+                        <input type="text" placeholder="Số điện thoại" required=""/>
+                        <FaPhone className='icon'/>
                     </div>
                     <div className='inputBox'>
                         <input type="password" placeholder="Mật khẩu" required=""/>
                         <FaLock className='icon'/>
                     </div>
-                    <div className='inputBox'>
-                        <input type="password" placeholder="Xác nhận mật khẩu" required=""/>
-                        <FaLock className='icon'/>
+                    <div className='inputBox2'>
+                        <input type="text" placeholder="Họ và tên" required=""/>
+                        <FaUser className='icon'/>
+                        <input type="text" placeholder="Ngày sinh" required=""/>
+                        <MdOutlineDateRange className='icon2'/>
                     </div>
+                    <div className='inputBox'>
+                        <input type="text" placeholder="Địa chỉ" required=""/>
+                        <FaHouse className='icon'/>
+                    </div>
+                    <div className='inputBox2'>
+                        <input type="text" placeholder="C.C. công dân" required=""/>
+                        <FaAddressCard className='icon3'/>
+                        <select required>
+                            <option value="">Lựa chọn</option>
+                            <option value="option1">Mẹ mày</option>
+                            <option value="option2">Béo</option>
+                            <option value="option3">Mẹ mày</option>
+                            <option value="option4">Béo</option>
+                            <option value="option5">Mẹ mày</option>
+                        </select>
+                    </div>
+                    <div className='register'></div>
                     <button onClick={handleRegister} type="button">Đăng ký</button>
 
                     <div className='register'></div>
