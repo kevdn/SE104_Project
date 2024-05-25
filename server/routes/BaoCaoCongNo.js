3cd865ec019dc0bae5b5d149bee6b150dfac22b4
Truong Quoc Khanh <162976496+qkhanh39@users.noreply.github.com>
2024-05-26 04:23:43 +0700

    Update BaoCaoCongNo.js

--- server/routes/BaoCaoCongNo.js
+++ server/routes/BaoCaoCongNo.js
@@ -2,9 +2,22 @@ const express = require("express")
 const router = express.Router();
 const {validateTokenNV} = require('../middlewares/AuthNhanVienMiddleware')
 const {validateTokenTP} = require('../middlewares/AuthTruongPhongMiddleware')
-
+const db = require('../middlewares/db');
 router.get('/BaoCaoCongNo', validateTokenTP, (req, res) => {
     return res.json("Valid");
+})
+
+router.get('/BaoCaoCongNoThang', (req, res) => {
+    const { month, year } = req.query;
+    const sql = "SELECT NHACUNGCAP.TenNhaCungCap, SUM(NHACUNGCAP.ConNo) FROM NHACUNGCAP, PHIEUNHAPHANG WHERE (MONTH(PHIEUNHAPHANG.NgayNhap) = ? AND YEAR(PHIEUNHAPHANG.NgayNhap) = ?) AND NHACUNGCAP.MaNhaCungCap = PHIEUNHAPHANG.MaNhaCungCap GRoup by NHACUNGCAP.TenNhaCungCap"
+    db.query(sql, [month, year], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result)
+            res.json(result);
+        }
+    })
 })
 
 module.exports = router;
