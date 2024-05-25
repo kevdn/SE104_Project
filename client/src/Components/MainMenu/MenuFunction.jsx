e960f7a63cda09064b6608d8f3c479e45c879d95
Khoa Le <53362871+kevdn@users.noreply.github.com>
2024-05-25 15:47:08 +0700

    Update MenuFunction.jsx

--- client/src/Components/MainMenu/MenuFunction.jsx
+++ client/src/Components/MainMenu/MenuFunction.jsx
@@ -1,60 +1,43 @@
-import { Menu } from "antd";
+import { Menu } from 'antd';
 import "./MainMenu.css";
-import { FaHome, FaUser, FaPowerOff } from "react-icons/fa";
+import { FaHome, FaUser, FaPowerOff } from 'react-icons/fa';
 import { RiCustomerServiceLine } from "react-icons/ri";
 import { IoStatsChart } from "react-icons/io5";
 import { MdElectricalServices, MdOutlineRule } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
 
 const MenuFunction = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div className="Menuwrapper">
-      <Menu
-        className="navbar"
-        onClick={({ key }) => {
-          navigate(key);
-        }}
-        items={[
-          { label: "Trang chủ", key: "/MainMenu", icon: <FaHome /> },
-          { label: "Khách hàng", key: "/Customers", icon: <FaUser /> },
-          {
-            label: "Thay đổi quy định",
-            key: "/ChangingRules",
-            icon: <MdOutlineRule />,
-          },
-          {
-            label: "Thiết bị",
-            key: "/Devices",
-            icon: <MdElectricalServices />,
-          },
-          {
-            label: "Dịch vụ",
-            key: "/Services",
-            icon: <RiCustomerServiceLine />,
-            children: [
-              { label: "Trả hàng", key: "/Service1" },
-              { label: "Sửa chữa", key: "/Service2" },
-              { label: "Bán hàng", key: "/Service3" },
-              { label: "Nhập hàng", key: "/Service4" },
-              { label: "Bảo hành", key: "/Service5" },
-            ],
-          },
-          {
-            label: "Thống kê",
-            key: "/Statistics",
-            icon: <IoStatsChart />,
-            children: [
-              { label: "Báo cáo công nợ", key: "/BaoCaoCongNo" },
-              { label: "Báo cáo tháng", key: "/BaoCaoThang" },
-            ],
-          },
-          { label: "Đăng xuất", key: "/", icon: <FaPowerOff />, danger: true },
-        ]}
-      ></Menu>
-    </div>
-  );
-};
+    return (
+            <div className='Menuwrapper'>
+                <Menu 
+                className='navbar'
+                onClick={({ key }) => {
+                    navigate(key);
+                }}
+                items={[
+                    { label: "Trang chủ", key:"/MainMenu", icon: <FaHome />},
+                    { label: "Khách hàng", key:"/Customers", icon: <FaUser />},
+                    { label: "Thay đổi quy định", key:"/ChangingRules", icon: <MdOutlineRule />},
+                    { label: "Thiết bị", key:"/Devices", icon: <MdElectricalServices />},
+                    { label: "Dịch vụ", key:"/Services", icon: <RiCustomerServiceLine />, children: [
+                        { label: "Trả hàng", key:"/TraHang" },
+                        { label: "Sửa chữa", key:"/SuaChua" },
+                        { label: "Bán hàng", key:"/BanHang" },
+                        { label: "Nhập hàng", key:"/NhapHang" },
+                        { label: "Bảo hành", key:"/BaoHanh" },
+                    ]},
+                    { label: "Thống kê", key:"/Statistics", icon: <IoStatsChart />, children: [
+                        { label: "Báo cáo công nợ", key:"/BaoCaoCongNo" },
+                        { label: "Báo cáo tháng", key:"/BaoCaoThang" },
+                    ]},
+                    { label: "Đăng xuất", key:"/", icon: <FaPowerOff />, danger: true},
+                    ]}>
+                </Menu>
+            </div>
+    );
+}
 
 export default MenuFunction;
