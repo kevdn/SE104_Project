9ac53a57a86e54a90a9015f498d8398faa4f63cd
Truong Quoc Khanh <162976496+qkhanh39@users.noreply.github.com>
2024-05-27 02:16:13 +0700

    Update ChangingRules.js

--- server/routes/ChangingRules.js
+++ server/routes/ChangingRules.js
@@ -1,10 +1,53 @@
 const express = require("express")
 const router = express.Router();
 const {validateTokenNV} = require('../middlewares/AuthNhanVienMiddleware')
-const {validateTokenTP} = require('../middlewares/AuthTruongPhongMiddleware')
+const {validateTokenTP} = require('../middlewares/AuthTruongPhongMiddleware');
+const db = require("../middlewares/db");
 
 router.get('/ChangingRules', validateTokenTP, (req, res) => {
     return res.json("Valid");
+})
+
+const upDateWayToPay = async (wayToPay) => {
+    const query = `INSERT INTO HINHTHUCTHANHTOAN (TenHTTT) VALUES ('${wayToPay}')`;
+    const [result] = await db.promise().query(query);
+    return result;
+}
+
+router.post('/ChangingWayToPay', async (req, res) => {
+    const wayToPay = req.body.wayToPay;
+    const selectedOption = req.body.selectedOption;
+    db.connect();
+    if (wayToPay === ""){
+        return res.json({err: "Vui lòng điền đầy đủ thông tin"});
+    }
+    
+    const result = await upDateWayToPay(wayToPay);
+    if (result.affectedRows > 0){
+        return res.json({success: true});
+    }
+    else {
+        return res.json({err: "Có lỗi xảy ra"});
+    }
+    db.close();
+})
+
+
+router.post('/ChangingProductType', async (req, res) => {
+    const productType = req.body.productType;
+    db.connect();
+    if (productType === ""){
+        return res.json({err: "Vui lòng điền đầy đủ thông tin"});
+    }
+    const query = `INSERT INTO LOAISANPHAM (TenLoaiSanPham, LoiNhuan) VALUES ('${productType}', 30)`;
+    const [result] = await db.promise().query(query);
+    if (result.affectedRows > 0){
+        return res.json({success: true});
+    }
+    else {
+        return res.json({err: "Có lỗi xảy ra"});
+    }
+    db.close();
 })
 
 module.exports = router;
