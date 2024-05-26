df51b7fad23a4928ee88cc71e266e092b5b5abfe
Khoa Le <53362871+kevdn@users.noreply.github.com>
2024-05-26 16:41:17 +0700

    Update Devices.js

--- server/routes/Devices.js
+++ server/routes/Devices.js
@@ -2,9 +2,21 @@ const express = require("express")
 const router = express.Router();
 const {validateTokenNV} = require('../middlewares/AuthNhanVienMiddleware')
 const {validateTokenTP} = require('../middlewares/AuthTruongPhongMiddleware')
+const db = require('../middlewares/db')
 
 router.get('/Devices', (validateTokenNV || validateTokenTP), (req, res) => {
     return res.json("Valid");
+})
+
+router.get('/Devices/typesOfDevices', (validateTokenNV || validateTokenTP), async (req, res) => {
+    //use sql query to get data from database
+    const query = "SELECT * FROM LOAISANPHAM";
+    db.query(query, (err, result) => {
+        if (err) {
+            return res.status(500).json({err: err});
+        }
+        return res.json(result);
+    });
 })
 
 module.exports = router;
