055626ab47664ac619a0db37a02d08475ceac767
Truong Quoc Khanh <162976496+qkhanh39@users.noreply.github.com>
2024-05-26 22:55:10 +0700

    Update Devices.js

--- server/routes/Devices.js
+++ server/routes/Devices.js
@@ -2,21 +2,39 @@ const express = require("express")
 const router = express.Router();
 const {validateTokenNV} = require('../middlewares/AuthNhanVienMiddleware')
 const {validateTokenTP} = require('../middlewares/AuthTruongPhongMiddleware')
-const db = require('../middlewares/db')
-
+const db = require('../middlewares/db');
 router.get('/Devices', (validateTokenNV || validateTokenTP), (req, res) => {
     return res.json("Valid");
 })
 
-router.get('/Devices/typesOfDevices', (validateTokenNV || validateTokenTP), async (req, res) => {
-    //use sql query to get data from database
-    const query = "SELECT * FROM LOAISANPHAM";
-    db.query(query, (err, result) => {
-        if (err) {
-            return res.status(500).json({err: err});
-        }
-        return res.json(result);
-    });
+router.get('/getDevices', (req, res) =>{
+    try{
+        const sql = "SELECT SANPHAM.TenSanPham, SANPHAM.MaLoaiSanPham, SANPHAM.GiaSanPham, NHACUNGCAP.TenNhaCungCap, SANPHAM.SoLuongTon, SANPHAM.SoLuongBan FROM SANPHAM, NHACUNGCAP, CT_PNH, PHIEUNHAPHANG WHERE SANPHAM.MaSanPham = CT_PNH.MaSanPham AND CT_PNH.MaPhieuNhap = PHIEUNHAPHANG.MaPhieuNhap AND PHIEUNHAPHANG.MaNhaCungCap = NHACUNGCAP.MaNhaCungCap";
+        db.query(sql, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(result);
+            }
+        })
+    } catch(err){
+        console.log(err);
+        res.json({err: "Lỗi khi lấy dữ liệu thiết bị"})
+    }
+})
+
+const getDevices = async (productType) => {
+    console.log(productType)
+    const sql = `SELECT SANPHAM.TenSanPham, SANPHAM.MaLoaiSanPham, SANPHAM.GiaSanPham, NHACUNGCAP.TenNhaCungCap, SANPHAM.SoLuongTon, SANPHAM.SoLuongBan FROM SANPHAM, NHACUNGCAP, CT_PNH, PHIEUNHAPHANG WHERE SANPHAM.MaSanPham = CT_PNH.MaSanPham AND CT_PNH.MaPhieuNhap = PHIEUNHAPHANG.MaPhieuNhap AND PHIEUNHAPHANG.MaNhaCungCap = NHACUNGCAP.MaNhaCungCap AND SANPHAM.MaLoaiSanPham = '${productType}'`;
+    const [result] = await db.promise().query(sql);
+    return result;
+}
+
+router.get("/findDevices", async (req, res) => {
+    const productType = req.query.productType
+    db.connect();
+    const data = await getDevices(productType);
+    res.json(data);
 })
 
 module.exports = router;
